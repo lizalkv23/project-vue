4b7d34607b50bd107fee3edec20e51ffa7ef652e
lizalkv23 <liza911991l@gmail.com>
2023-06-30 16:41:33 +0300

    added components from Primevue

--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
-import { createApp } from "vue"
-import App from "./App.vue"
-import router from "./router"
-import PrimeVue from "primevue/config"
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import PrimeVue from 'primevue/config'
 
-import "primevue/resources/themes/lara-light-indigo/theme.css"
-import "primevue/resources/primevue.min.css"
-import "primeicons/primeicons.css"
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import Sidebar from 'primevue/sidebar'
+import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
+import InputText from 'primevue/inputtext'
+import Rating from 'primevue/rating'
+import '/node_modules/primeflex/primeflex.css'
+import DataView from 'primevue/dataview'
+import DataViewLayoutOptions from 'primevue/dataviewlayoutoptions'
+import Tag from 'primevue/tag'
+import Dropdown from 'primevue/dropdown'
 const app = createApp(App)
 
 app.use(PrimeVue)
-// app.component("Button", Button)
-// app.component("Sidebar", Sidebar)
-// app.component("Menubar", Menubar)
-
+app.component('Button', Button)
+app.component('Sidebar', Sidebar)
+app.component('Dialog', Dialog)
+app.component('InputText', InputText)
+app.component('DataView', DataView)
+app.component('DataViewLayoutOptions', DataViewLayoutOptions)
+app.component('Rating', Rating)
+app.component('Tag', Tag)
+app.component('Dropdown', Dropdown)
 app.use(router)
-app.mount("#app")
+app.mount('#app')
