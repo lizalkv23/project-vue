e827e7269e5088933a3e9d872f3ada4648141643
lizalkv23 <liza911991l@gmail.com>
2023-06-30 16:44:46 +0300

    added new component sidebar-right

---
+++ src/components/UX/SidebarRight.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="fixed top-50 right-0">
+      <Button icon="pi pi-comments text-cyan-200" class="info" @click="visibleRight = true" />
+    </div>
+    <Sidebar v-model:visible="visibleRight" position="right" class="sidebar">
+      <h2>Right Sidebar</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+    </Sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visibleRight: false
+    }
+  }
+}
+</script>
