d0370ed8ff8d6b4141c403500414406b2d12c22a
lizalkv23 <liza911991l@gmail.com>
2023-06-30 16:51:55 +0300

    changed paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,39 @@
-import { createRouter, createWebHistory } from "vue-router"
-import HomeView from "@/views/HomeView.vue"
-import GalleryView from "@/views/GalleryView.vue"
-import AboutView from "@/views/AboutView.vue"
-import ContactsView from "@/views/ContactsView.vue"
-import HelpView from "@/views/HelpView.vue"
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import GalleryView from '@/views/GalleryView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ContactsView from '@/views/ContactsView.vue'
+import DataView from '@/views/DataView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "Home",
-      component: HomeView,
+      path: '/',
+      name: 'Home',
+      component: HomeView
     },
     {
-      path: "/gallery",
-      name: "GalleryView",
-      component: GalleryView,
+      path: '/gallery',
+      name: 'GalleryView',
+      component: GalleryView
     },
     {
-      path: "/about",
-      name: "AboutView",
-      component: AboutView,
+      path: '/about',
+      name: 'AboutView',
+      component: AboutView
     },
     {
-      path: "/contacts",
-      name: "ContactsView",
-      component: ContactsView,
+      path: '/contacts',
+      name: 'ContactsView',
+      component: ContactsView
     },
     {
-      path: "/help",
-      name: "HelpView",
-      component: HelpView,
-    },
-  ],
+      path: '/data',
+      name: 'DataView',
+      component: DataView
+    }
+  ]
 })
 
 export default router
