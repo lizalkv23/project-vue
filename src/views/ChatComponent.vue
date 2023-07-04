f139882ea809a7d2d6ccdfcd5b25bfab855dd765
lizalkv23 <liza911991l@gmail.com>
2023-07-04 15:54:45 +0300

    Made a new bar chart component A bar chart or bar graph is a chart that presents grouped data with rectangular bars

---
+++ src/views/ChatComponent.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="card">
+    <Chart type="bar" :data="chartData" :options="chartOptions" class="h-30rem " />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartData: null,
+      chartOptions: null
+    };
+  },
+  mounted() {
+    this.chartData = this.setChartData();
+    this.chartOptions = this.setChartOptions();
+  },
+  methods: {
+    setChartData() {
+      const documentStyle = getComputedStyle(document.documentElement);
+
+      return {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            backgroundColor: documentStyle.getPropertyValue('--green-500'),
+            borderColor: documentStyle.getPropertyValue('--green-500'),
+            data: [65, 59, 80, 81, 56, 55, 40]
+          },
+          {
+            label: 'My Second dataset',
+            backgroundColor: documentStyle.getPropertyValue('--indigo-600'),
+            borderColor: documentStyle.getPropertyValue('--indigo-6000'),
+            data: [28, 48, 40, 19, 86, 27, 90]
+          }
+        ]
+      };
+    },
+    setChartOptions() {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      return {
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+          legend: {
+            labels: {
+              fontColor: textColor
+            }
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+              font: {
+                weight: 500
+              }
+            },
+            grid: {
+              display: false,
+              drawBorder: false
+            }
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder,
+              drawBorder: false
+            }
+          }
+        }
+      };
+    }
+  }
+};
+</script>
