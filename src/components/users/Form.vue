9114e6115fe7720952741cd1a20e4eba5edcc681
lizalkv23 <liza911991l@gmail.com>
2023-07-11 13:27:22 +0300

    Created new components.

---
+++ src/components/users/Form.vue
@@ -0,0 +1,13 @@
+<template>
+   <div>
+      <h1>Form</h1>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'form'
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/users/Show.vue
@@ -0,0 +1,13 @@
+<template>
+   <div>
+      <h1>Show</h1>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'show'
+}
+</script>
+
+<style lang="scss" scoped></style>
