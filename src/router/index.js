01b5b13b573571bc3d34e53fdcb3d3512c613691
lizalkv23 <liza911991l@gmail.com>
2023-07-11 13:32:27 +0300

    Added new routes, changed the logic of importing files

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import GalleryView from '@/views/GalleryView.vue'
-import AboutView from '@/views/AboutView.vue'
-import ChatComponent from '@/views/ChatComponent.vue'
-import DataView from '@/views/DataView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,28 +6,83 @@ const router = createRouter({
     {
       path: '/',
       name: 'Home',
-      component: HomeView
+      meta: {
+        title: "Home"
+      },
+      component: () => import('@/views/HomeView.vue')
     },
     {
       path: '/gallery',
-      name: 'GalleryView',
-      component: GalleryView
+      name: 'gallery',
+      meta: {
+        title: "Gallery"
+      },
+      component: () => import('@/views/GalleryView.vue')
     },
     {
       path: '/about',
-      name: 'AboutView',
-      component: AboutView
+      name: 'about',
+      meta: {
+        title: "About"
+      },
+      component: () => import('@/views/AboutView.vue')
     },
     {
       path: '/chat',
-      name: 'ChatComponent',
-      component: ChatComponent
+      name: 'chat',
+      meta: {
+        title: "Chat"
+      },
+      component: () => import('@/views/ChatComponent.vue')
     },
     {
       path: '/data',
       name: 'DataView',
-      component: DataView
-    }
+      meta: {
+        title: "List"
+      },
+      component: () => import('@/views/DataView.vue')
+    },
+    {
+      path: '/list',
+      name: 'list',
+      meta: {
+        title: "List"
+      },
+      component: () => import('@/components/users/List.vue')
+    },
+    {
+      path: '/show',
+      name: 'show',
+      meta: {
+        title: "Show"
+      },
+      component: () => import('@/components/users/Show.vue')
+    },
+    {
+      path: '/create',
+      name: 'create',
+      meta: {
+        title: "Create"
+      },
+      component: () => import('@/components/users/Create.vue')
+    },
+    {
+      path: '/form',
+      name: 'form',
+      meta: {
+        title: "Form"
+      },
+      component: () => import('@/components/users/Form.vue')
+    },
+    {
+      path: '/update',
+      name: 'update',
+      meta: {
+        title: "Update"
+      },
+      component: () => import('@/components/users/Update.vue')
+    },
   ]
 })
 
