e13df8c4e15a357bb00d0c324062f1a84e5c359d
lizalkv23 <liza911991l@gmail.com>
2023-07-13 14:29:48 +0300

    Added <router-view> and removed the inside component

---
+++ src/views/admin/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+import MainApp from "@/components/layout/MainApp.vue"
+export default {
+  name: 'home',
+  components: {
+    MainApp,
+  },
+}
+</script>
+
+<style scoped></style>
